d7208fd9924540986cfa5cc075142ff0eedd0a24
liguorui <liguoruixy@qq.com>
2014-06-12 10:49:47 +0800

    合并版本

--- beatyHelp/ManageController.swift
+++ beatyHelp/ManageController.swift
@@ -9,20 +9,10 @@
 import Foundation
 import UIKit
 
-class ManageController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    var pubTable : UITableView?
-    var getTable : UITableView?
-    
+class ManageController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< HEAD
-        setPubTable()
-        setGetTable()
-        manageViewDraw(self)
-        
-=======
         ManageViewDraw(_controller: self)
->>>>>>> FETCH_HEAD
     }
     
     override func didReceiveMemoryWarning() {
@@ -34,64 +24,6 @@ class ManageController: UIViewController, UITableViewDelegate, UITableViewDataSo
         return UIStatusBarStyle.LightContent
     }
     
-    func setPubTable(){
-        self.pubTable = UITableView(frame:CGRectMake(7,70,306,UIScreen.mainScreen().applicationFrame.height-71), style:UITableViewStyle.Plain)
-        self.pubTable!.delegate = self
-        self.pubTable!.dataSource = self
-        self.pubTable!.rowHeight = 125
-        self.pubTable!.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        self.pubTable!.backgroundColor = UIColor.clearColor()
-        self.pubTable!.separatorColor = UIColor.clearColor()
-        self.automaticallyAdjustsScrollViewInsets = false
-        self.view?.addSubview(self.pubTable)
-    }
-    
-    func setGetTable(){
-        self.getTable = UITableView(frame:CGRectMake(7,70,306,UIScreen.mainScreen().applicationFrame.height-71), style:UITableViewStyle.Plain)
-        self.getTable!.delegate = self
-        self.getTable!.dataSource = self
-        self.getTable!.rowHeight = 125
-        self.getTable!.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        self.getTable!.backgroundColor = UIColor.clearColor()
-        self.getTable!.separatorColor = UIColor.clearColor()
-        self.automaticallyAdjustsScrollViewInsets = false
-        self.view?.addSubview(self.getTable)
-    }
-    
-    // UITableViewDataSource Methods
-    func numberOfSectionsInTableView(tableView: UITableView!) -> Int
-    {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
-    {
-        return 12
-    }
-    
-    //创建一个单元格
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
-    {
-        if tableView == self.pubTable{
-            return getPubTableView(tableView,cellForRowAtIndexPath:indexPath,0)
-        }
-        else{
-            return getPubTableView(tableView,cellForRowAtIndexPath:indexPath,1)
-        }
-    }
-    
-    // UITableViewDelegate Methods
-    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!)
-    {
-        if tableView==self.pubTable{
-            self.pubTable!.deselectRowAtIndexPath(indexPath, animated: false)
-        }
-        else if tableView==self.getTable{
-            self.getTable!.deselectRowAtIndexPath(indexPath, animated: false)
-        }
-    }
-    
-    
     func footBtn1Action(sender: UIButton!) {
         var ViewCon = ViewController()
         self.presentModalViewController(ViewCon, animated:false)
@@ -100,18 +32,6 @@ class ManageController: UIViewController, UITableViewDelegate, UITableViewDataSo
     func footBtn3Action(sender: UIButton!) {
         var myDataCon = MyDataController()
         self.presentModalViewController(myDataCon, animated:false)
-    }
-    
-    func leftTabAction(sender: UIButton!) {
-        sender.alpha=0.5
-        self.pubTable!.hidden=false;
-        self.getTable!.hidden=true;
-    }
-    
-    func rightTabAction(sender: UIButton!) {
-        sender.alpha=0.5
-        self.pubTable!.hidden=true;
-        self.getTable!.hidden=false;
     }
     
 }
--- beatyHelp/MyDataController.swift
+++ beatyHelp/MyDataController.swift
@@ -10,7 +10,6 @@ import Foundation
 import UIKit
 
 class MyDataController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         MyDataViewDraw(_controller: self)
